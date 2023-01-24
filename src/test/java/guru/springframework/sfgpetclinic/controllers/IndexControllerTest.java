1c2e386174dad379ed8262364d421c4729e71196
Ernesto Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-01-24 23:29:32 +0000

    Testing timeouts

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,8 +1,11 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,5 +36,25 @@ class IndexControllerTest {
 //        assertTrue("notimplemented".equals(controller.oopsHandler()), () -> "This is some expensive " +
 //                "Message tp build " +
 //                "for my test");
+    }
+
+    @Disabled("Demo of timeout")
+    @Test
+    void testTimeout(){
+        assertTimeout(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+
+            System.out.println("I got here");
+        });
+    }
+
+    @Disabled("Demo of timeout")
+    @Test
+    void testTimeoutPreemptively(){
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+
+            System.out.println("I should not got here");
+        });
     }
 }
